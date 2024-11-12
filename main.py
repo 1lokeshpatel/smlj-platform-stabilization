630f89b6aae79b03368b0ed5e4d36d92311952ab
Lokesh Patel <64927810+LowCASHH@users.noreply.github.com>
2024-11-12 18:07:59 -0500

    test1

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ try:
         print("Motor setup failed. Exiting...")
         quit()
 
-    for i in range(10):
-        ctrl.move_motor(motor_control.angle_to_position(190), 
-                        motor_control.angle_to_position(190), 
-                        motor_control.angle_to_position(190))
+    time.sleep(5)
+    # for i in range(10):
+    #     ctrl.move_motor(motor_control.angle_to_position(190), 
+    #                     motor_control.angle_to_position(190), 
+    #                     motor_control.angle_to_position(190))
 
-        ctrl.move_motor(motor_control.angle_to_position(240), 
-                        motor_control.angle_to_position(240), 
-                        motor_control.angle_to_position(240))
+    #     ctrl.move_motor(motor_control.angle_to_position(240), 
+    #                     motor_control.angle_to_position(240), 
+    #                     motor_control.angle_to_position(240))
 
 except Exception as e:
     print(f"An error occurred: {e}")
