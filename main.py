e28f54f5092bfc464a93dd558efbb233aebc1479
Lokesh Patel <64927810+LowCASHH@users.noreply.github.com>
2024-11-12 18:01:48 -0500

    changing limits

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ try:
         quit()
 
     for i in range(10):
-        ctrl.move_motor(motor_control.angle_to_position(210), 
-                        motor_control.angle_to_position(210), 
-                        motor_control.angle_to_position(210))
+        ctrl.move_motor(motor_control.angle_to_position(190), 
+                        motor_control.angle_to_position(190), 
+                        motor_control.angle_to_position(190))
 
-        ctrl.move_motor(motor_control.angle_to_position(260), 
-                        motor_control.angle_to_position(260), 
-                        motor_control.angle_to_position(260))
+        ctrl.move_motor(motor_control.angle_to_position(240), 
+                        motor_control.angle_to_position(240), 
+                        motor_control.angle_to_position(240))
 
 except Exception as e:
     print(f"An error occurred: {e}")
