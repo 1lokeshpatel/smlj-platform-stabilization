82642579f454394f83740ff1f59167288f9e1d50
Dwbcan <dew.bhaumik8@gmail.com>
2024-11-19 00:56:27 -0500

    47

--- main.py
+++ main.py
@@ -74,14 +74,15 @@ try:
     cam_thread.start()
 
     while(True):
-        print("Main loop running")
-        Current_value = [x, y, area]
+        # print("Main loop running")
+        # Current_value = [x, y, area]
 
-        if x != -1:
-            theta, phi = pid.calc(goal, Current_value)
+        # if x != -1:
+        #     theta, phi = pid.calc(goal, Current_value)
 
-        new_position = [theta, phi, robot.starting_position[2]]
-        robot.adjust_posture(new_position, 0.01)
+        test = 0
+        # new_position = [theta, phi, robot.starting_position[2]]
+        # robot.adjust_posture(new_position, 0.01)
 
 except Exception as e:
     print(f"An error occurred: {e}")
