bc2ec4e5d650b3fb966d1659c94ad501683a0bd4
Lokesh Patel <64927810+LowCASHH@users.noreply.github.com>
2024-11-19 00:34:33 -0500

    speed * 1000

--- motor_control.py
+++ motor_control.py
@@ -111,9 +111,9 @@ class MotorControl:
         self.dxl2_initial_position = dxl2_present_position+5
         self.dxl3_initial_position = dxl3_present_position+5
 
-        self.set_velocity_profile(DXL1_ID, 6)
-        self.set_velocity_profile(DXL2_ID, 6)    
-        self.set_velocity_profile(DXL3_ID, 6)    
+        self.set_velocity_profile(DXL1_ID, 24)
+        self.set_velocity_profile(DXL2_ID, 24)    
+        self.set_velocity_profile(DXL3_ID, 24)    
 
     def move_motor(self, motorPos1, motorPos2, motorPos3=0):
 
