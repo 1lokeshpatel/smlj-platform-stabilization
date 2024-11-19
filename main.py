8cf09cce056c7a08ed4265aedfa7f8eddff99f42
Dwbcan <dew.bhaumik8@gmail.com>
2024-11-19 00:03:52 -0500

    31

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ def get_cam_feed():
     while(True):
         # Capture and process each frame
         frame = camera.capture_image()
-        print(frame)
         if frame is None:
             break  # Stop if frame capture fails
         camera.display_video(frame)
