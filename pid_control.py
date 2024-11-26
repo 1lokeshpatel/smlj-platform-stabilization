cfb0607e7b433b44b7b037ea4ea1cd02dfe2836b
Dwbcan <dew.bhaumik8@gmail.com>
2024-11-25 20:32:21 -0500

    Print PID errors

--- pid_control.py
+++ pid_control.py
@@ -26,6 +26,7 @@ class PID:
         # Calculate the current errors in x and y
         curr_err_x = goal[0] - curr_val[0]
         curr_err_y = goal[1] - curr_val[1]
+
         time_diff = curr_time - self.prev_time
         self.integral_x += curr_err_x * time_diff
         self.integral_y += curr_err_y * time_diff
@@ -37,6 +38,9 @@ class PID:
         # PID control for x (roll) and y (pitch)
         roll = self.kp * curr_err_x + self.ki * self.integral_x + self.kd * derivative_x
         pitch = self.kp * curr_err_y + self.ki * self.integral_y + self.kd * derivative_y
+
+        print(f"X Error: {curr_err_x}   Kp_X Error: {self.kp * curr_err_x}   Ki_X Error: {self.ki * self.integral_x}   Kd_X Error: {self.kd * derivative_x}")
+        print(f"Y Error: {curr_err_y}   Kp_Y Error: {self.kp * curr_err_y}   Ki_Y Error: {self.ki * self.integral_y}   Kd_Y Error: {self.kd * derivative_y}")
 
         # Apply smoothing (low pass filter)
         # roll = self.alpha * roll + (1 - self.alpha) * self.prev_x
